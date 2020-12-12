7c2eafab1dc41c6e79539553f8769a04165190d8
Kevin Thomas <kevin@mytechnotalent.com>
2020-12-12 08:47:34 -0500

    Update p_0001_candy_name_generator_ec.py

--- Part_1_Basic_IO/p_0001_candy_name_generator_ec.py
+++ Part_1_Basic_IO/p_0001_candy_name_generator_ec.py
@@ -4,7 +4,7 @@ from speech import say
 SPEED = 95
 
 candy_title = input('What is the candy title? ')
-candy_flavor = input('What is the candy flavor ')
+candy_flavor = input('What is the candy flavor? ')
 
 display.show(Image.SURPRISED)
 print('It shall be called {0} {1}!'.format(candy_title, candy_flavor))

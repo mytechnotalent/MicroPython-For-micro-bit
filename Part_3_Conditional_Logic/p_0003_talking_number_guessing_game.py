cf532f6510c63b9e6dcad96311263eb6692e5d56
Kevin Thomas <kevin@mytechnotalent.com>
2020-12-26 08:27:51 -0500

    Add files for Part 3

---
+++ Part_3_Conditional_Logic/p_0003_talking_number_guessing_game.py
@@ -0,0 +1,41 @@
+from random import randint
+from microbit import display, Image, button_a, button_b, pin_logo
+from speech import say
+
+random_number = randint(1, 9)
+
+# Create number_position and init to 1
+number_position = 1
+
+display.show(Image.SURPRISED)
+say('Pick a number between 1 and 9.')
+display.show(Image.HAPPY)
+
+while True:
+    display.show(number_position)
+    
+    if button_a.was_pressed():
+        if number_position == 9:
+            pass
+        else:
+            number_position += 1
+            display.scroll(number_position)
+            
+    if button_b.was_pressed():
+        if number_position == 1:
+            pass
+        else:
+            number_position -= 1
+            display.scroll(number_position)
+     
+    if pin_logo.is_touched():
+        if number_position == random_number:
+            display.show(Image.SURPRISED)
+            say('Correct!')
+            display.show(Image.HAPPY)
+            break
+        else:
+            display.show(Image.SURPRISED)
+            say('The number I chose is {0}.'.format(random_number))
+            display.show(Image.HAPPY)
+            break

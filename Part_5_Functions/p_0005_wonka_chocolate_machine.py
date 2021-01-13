df5b9aec8ad2e203a1587a071a9be07272d5daaf
Kevin Thomas <kevin@mytechnotalent.com>
2021-01-13 18:59:26 -0500

    Update p_0005_wonka_chocolate_machine.py

--- Part_5_Functions/p_0005_wonka_chocolate_machine.py
+++ Part_5_Functions/p_0005_wonka_chocolate_machine.py
@@ -48,15 +48,15 @@ CHOCOLATE_CHOICES = {
 }
 
 raw_materials = {
-    'sugar': 5,
-    'butter': 5,
+    'sugar': 2,
+    'butter': 2,
     'caramel': 15,
     'dark chocolate': 30,
     'mint chocolate': 30,
     'milk chocolate': 30,
-    'light corn syrup': 5,
-    'sweetened condensed milk': 5,
-    'vanilla extract': 5,
+    'light corn syrup': 2,
+    'sweetened condensed milk': 2,
+    'vanilla extract': 2,
     'Reese\'s Pieces': 15,
 }
 
@@ -67,20 +67,18 @@ SHUTDOWN_PASSWORD = '8675309'
 def has_raw_materials(f_raw_materials):
     """Check if there are enough raw materials in the machine
 
-    Args:
+    Params:
         f_raw_materials: dict
 
     Returns:
-        True or False
+        str or True
     """
-    need_raw_materials = False
+    additional_resources_needed = ''
     for f_raw_material in f_raw_materials:
         if f_raw_materials[f_raw_material] > raw_materials[f_raw_material]:
-            need_raw_materials = True
-            print('Machine Needs Additional: {0}'.format(f_raw_material))
-    if need_raw_materials:
-        print()
-        return False
+            additional_resources_needed += 'Machine Needs Additional: {0}\n'.format(f_raw_material)
+    if additional_resources_needed:
+        return additional_resources_needed
     else:
         return True
 
@@ -88,72 +86,79 @@ def has_raw_materials(f_raw_materials):
 def collect_money(f_max_value):
     """Collect money into the machine
 
-    Args:
+    Params:
         f_max_value: float
 
     Returns:
-        float or False
+        float or str
     """
     try:
         money_collected = int(input('Quarters: ')) * 0.25
         money_collected += int(input('Dimes: ')) * 0.10
         money_collected += int(input('Nickels: ')) * 0.05
         if money_collected <= 0.00:
-            print('Insufficient funds...  Dispensing coins inserted.')
+            return 'Insufficient funds...  Dispensing coins inserted.'
         elif money_collected >= f_max_value:
-            print('Machine can\'t hold more than ${0:.2f}...  Dispensing coins inserted.'.format(f_max_value))
+            return 'Machine can\'t hold more than ${0:.2f}...  Dispensing coins inserted.'.format(f_max_value)
         else:
             return money_collected
     except ValueError:
-        print('Please enter valid currency.')
-        return False
+        return 'Please enter valid currency.\n'
 
 
 def has_enough_money(f_money_collected, f_chocolate_price):
     """Check to see if customer put in enough money into the machine
 
-    Args:
+    Params:
         f_money_collected: float
         f_chocolate_price: float
 
-    Return:
-        True or False
+    Returns:
+        str
     """
     if f_money_collected >= f_chocolate_price:
         excess_money_collected = round(f_money_collected - f_chocolate_price, 2)
-        print('Change: ${0:.2f}'.format(excess_money_collected))
         global total_money_collected
         total_money_collected += f_chocolate_price
-        return True
+        return 'Change: ${0:.2f}\n'.format(excess_money_collected)
     else:
-        print('Insufficient funds...  Dispensing coins inserted.')
-        return False
+        return 'Insufficient funds...  Dispensing coins inserted.\n'
 
 
 def bake_chocolate_bar(f_chocolate_choice, f_raw_materials):
     """Bake chocolate bar from raw materials
 
-    Args:
-        f_chocolate_choice: dict
+    Params:
+        f_chocolate_choice: str
         f_raw_materials: dict
+
+    Returns:
+        str
     """
     for f_raw_material in f_raw_materials:
         raw_materials[f_raw_material] -= f_raw_materials[f_raw_material]
-    print('A {0} chocolate bar dispensed!\n'.format(f_chocolate_choice))
+    return 'A {0} chocolate bar dispensed!'.format(f_chocolate_choice)
 
 
 def stats():
-    """Show machine statistics"""
-    print('sugar {0} tablespoons remaining'.format(raw_materials['sugar']))
-    print('butter {0} teaspoons remaining'.format(raw_materials['butter']))
-    print('dark chocolate {0} tablespoons remaining'.format(raw_materials['dark chocolate']))
-    print('mint chocolate {0} tablespoons remaining'.format(raw_materials['mint chocolate']))
-    print('milk chocolate {0} tablespoons remaining'.format(raw_materials['milk chocolate']))
-    print('light corn syrup {0} teaspoons remaining'.format(raw_materials['light corn syrup']))
-    print('sweetened condensed milk {0} teaspoons remaining'.format(raw_materials['sweetened condensed milk']))
-    print('vanilla extract {0} teaspoons remaining'.format(raw_materials['vanilla extract']))
-    print('Reese\'s Pieces {0} tablespoons remaining'.format(raw_materials['Reese\'s Pieces']))
-    print('Total Money Collected: ${0:.2f}\n'.format(total_money_collected))
+    """
+    Show machine statistics
+
+    Returns:
+        str
+    """
+    cm_stats = 'sugar {0} tablespoons remaining\n'.format(raw_materials['sugar'])
+    cm_stats += 'butter {0} teaspoons remaining\n'.format(raw_materials['butter'])
+    cm_stats += 'dark chocolate {0} tablespoons remaining\n'.format(raw_materials['dark chocolate'])
+    cm_stats += 'mint chocolate {0} tablespoons remaining\n'.format(raw_materials['mint chocolate'])
+    cm_stats += 'milk chocolate {0} tablespoons remaining\n'.format(raw_materials['milk chocolate'])
+    cm_stats += 'light corn syrup {0} teaspoons remaining\n'.format(raw_materials['light corn syrup'])
+    cm_stats += 'sweetened condensed milk {0} teaspoons remaining\n'.format(raw_materials[
+        'sweetened condensed milk'])
+    cm_stats += 'vanilla extract {0} teaspoons remaining\n'.format(raw_materials['vanilla extract'])
+    cm_stats += 'Reese\'s Pieces {0} tablespoons remaining\n'.format(raw_materials['Reese\'s Pieces'])
+    cm_stats += 'Total Money Collected: ${0:.2f}\n'.format(total_money_collected)
+    return cm_stats
 
 
 machine_active = True
@@ -173,14 +178,26 @@ while machine_active:
         else:
             print('YOU ARE NOT AUTHORIZED TO DISABLE THIS MACHINE!\n')
     elif choice == 'stats':
-        stats()
+        stats_ = stats()
+        print(stats_)
     elif valid_choice:
         selection = CHOCOLATE_CHOICES[choice]
-        if has_raw_materials(selection['ingredients']):
-            customer_money_collected = collect_money(100.00)
-            if customer_money_collected:
-                if has_enough_money(customer_money_collected, selection['price']):
-                    bake_chocolate_bar(choice, selection['ingredients'])
+        has_enough_raw_materials = has_raw_materials(selection['ingredients'])
+        if not isinstance(has_enough_raw_materials, bool):
+            print(has_enough_raw_materials)
+            machine_active = False
+        if isinstance(has_enough_raw_materials, bool):
+            money = collect_money(100.00)
+            if not isinstance(money, float):
+                print(money)
+            else:
+                change = has_enough_money(money, selection['price'])
+                if change == 'Insufficient funds...  Dispensing coins inserted.\n':
+                    print(change)
+                else:
+                    chocolate_bar = bake_chocolate_bar(choice, selection['ingredients'])
+                    print(chocolate_bar)
+                    print(change)
         else:
             machine_active = False
 

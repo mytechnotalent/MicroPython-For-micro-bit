cad0d0a3988574cc2b226ac260f7d772da8d0cb4
Kevin Thomas <kevin@mytechnotalent.com>
2021-02-12 14:31:49 -0500

    Update funcs.py

--- Part_5_Functions/p_0005_wonka_chocolate_machine/funcs.py
+++ Part_5_Functions/p_0005_wonka_chocolate_machine/funcs.py
@@ -21,7 +21,7 @@ def has_raw_materials(f_raw_materials, d_raw_materials):
         return True
 
 
-def collect_money(f_max_value, m_quarters, m_dimes, m_nickels):
+def collect_money(f_max_value, f_quarters, f_dimes, f_nickels):
     """Collect money into the machine
 
     Params:
@@ -31,9 +31,9 @@ def collect_money(f_max_value, m_quarters, m_dimes, m_nickels):
         float or str
     """
     try:
-        money_collected = int(m_quarters) * 0.25
-        money_collected += int(m_dimes) * 0.10
-        money_collected += int(m_nickels) * 0.05
+        money_collected = int(f_quarters) * 0.25
+        money_collected += int(f_dimes) * 0.10
+        money_collected += int(f_nickels) * 0.05
         if money_collected <= 0.00:
             return 'Insufficient funds...  Dispensing coins inserted.'
         elif money_collected >= f_max_value:

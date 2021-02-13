22025e16feb1077230c5ac5a2faca9ef40ea6449
Kevin Thomas <kevin@mytechnotalent.com>
2021-02-13 07:57:07 -0500

    Update ChocolateMachine.py

--- Part_6_Classes/p_0006_wonka_chocolate_machine/ChocolateMachine.py
+++ Part_6_Classes/p_0006_wonka_chocolate_machine/ChocolateMachine.py
@@ -1,5 +1,4 @@
-from Machine import Machine
-from data import raw_materials
+from lib.Machine import Machine
 
 
 class ChocolateMachine(Machine):
@@ -7,47 +6,52 @@ class ChocolateMachine(Machine):
     Child class to represent a chocolate machine inheriting from the Machine base class
     """
 
-    def __init__(self, choices):
+    def __init__(self, choices, __total_money_collected=0):
         """
         Attrs:
             choices: str
+            __total_money_collected: float
         """
-        super().__init__()
         self.choices = choices
+        super().__init__()
 
-    def stats(self):
+    def stats(self, d_raw_materials=None):
         """
         Show machine statistics
+
+        Params:
+            d_raw_materials: dict
 
         Returns:
             str
         """
-        cm_stats = 'sugar {0} tablespoons remaining\n'.format(raw_materials['sugar'])
-        cm_stats += 'butter {0} teaspoons remaining\n'.format(raw_materials['butter'])
-        cm_stats += 'dark chocolate {0} tablespoons remaining\n'.format(raw_materials['dark chocolate'])
-        cm_stats += 'mint chocolate {0} tablespoons remaining\n'.format(raw_materials['mint chocolate'])
-        cm_stats += 'milk chocolate {0} tablespoons remaining\n'.format(raw_materials['milk chocolate'])
-        cm_stats += 'light corn syrup {0} teaspoons remaining\n'.format(raw_materials['light corn syrup'])
-        cm_stats += 'sweetened condensed milk {0} teaspoons remaining\n'.format(raw_materials[
+        cm_stats = 'sugar {0} tablespoons remaining\n'.format(d_raw_materials['sugar'])
+        cm_stats += 'butter {0} teaspoons remaining\n'.format(d_raw_materials['butter'])
+        cm_stats += 'dark chocolate {0} tablespoons remaining\n'.format(d_raw_materials['dark chocolate'])
+        cm_stats += 'mint chocolate {0} tablespoons remaining\n'.format(d_raw_materials['mint chocolate'])
+        cm_stats += 'milk chocolate {0} tablespoons remaining\n'.format(d_raw_materials['milk chocolate'])
+        cm_stats += 'light corn syrup {0} teaspoons remaining\n'.format(d_raw_materials['light corn syrup'])
+        cm_stats += 'sweetened condensed milk {0} teaspoons remaining\n'.format(d_raw_materials[
             'sweetened condensed milk'])
-        cm_stats += 'vanilla extract {0} teaspoons remaining\n'.format(raw_materials['vanilla extract'])
-        cm_stats += 'Reese\'s Pieces {0} tablespoons remaining\n'.format(raw_materials['Reese\'s Pieces'])
+        cm_stats += 'vanilla extract {0} teaspoons remaining\n'.format(d_raw_materials['vanilla extract'])
+        cm_stats += 'Reese\'s Pieces {0} tablespoons remaining\n'.format(d_raw_materials['Reese\'s Pieces'])
         cm_stats += super(ChocolateMachine, self).stats()
         return cm_stats
 
     @staticmethod
-    def has_raw_materials(m_raw_materials):
+    def has_raw_materials(m_raw_materials, d_raw_materials):
         """Check if there are enough raw materials in the machine
 
         Params:
             m_raw_materials: dict
+            d_raw_materials: dict
 
         Returns:
             str or bool
         """
         additional_resources_needed = ''
         for m_raw_material in m_raw_materials:
-            if m_raw_materials[m_raw_material] > raw_materials[m_raw_material]:
+            if m_raw_materials[m_raw_material] > d_raw_materials[m_raw_material]:
                 additional_resources_needed += 'Machine Needs Additional: {0}\n'.format(m_raw_material)
         if additional_resources_needed:
             return additional_resources_needed
@@ -55,7 +59,7 @@ class ChocolateMachine(Machine):
             return True
 
     @staticmethod
-    def bake_chocolate_bar(chocolate_choice, m_raw_materials):
+    def bake_chocolate_bar(chocolate_choice, m_raw_materials, d_raw_materials):
         """
         Bake chocolate bar from raw materials
 
@@ -67,6 +71,5 @@ class ChocolateMachine(Machine):
             str
         """
         for m_raw_material in m_raw_materials:
-            raw_materials[m_raw_material] -= m_raw_materials[m_raw_material]
+            d_raw_materials[m_raw_material] -= m_raw_materials[m_raw_material]
         return 'A {0} chocolate bar dispensed!'.format(chocolate_choice)
-
